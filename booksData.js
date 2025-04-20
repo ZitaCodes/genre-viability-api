5d9be63b8a8591620d128dab1c11d5705c0307bc
ZitaCodes <zita@zitacodes.tech>
2025-04-20 17:40:30 -0400

    Create booksData.js

---
+++ booksData.js
@@ -0,0 +1,76 @@
+const booksData = [
+  {
+    title: "Alien Seduction",
+    author: "Ruby Star",
+    genre: "insta heat alien romance",
+    price: 4.99,
+    pages: 285,
+    in_ku: true
+  },
+  {
+    title: "Mated by Moonlight",
+    author: "S.J. Sanders",
+    genre: "cozy paranormal fae romance",
+    price: 3.99,
+    pages: 275,
+    in_ku: true
+  },
+  {
+    title: "His Dark Mark",
+    author: "Ella Maven",
+    genre: "dark mafia paranormal",
+    price: 5.99,
+    pages: 300,
+    in_ku: false
+  },
+  {
+    title: "Forbidden Flame",
+    author: "Honey Phillips",
+    genre: "age gap sci-fi romance",
+    price: 4.49,
+    pages: 290,
+    in_ku: true
+  },
+  {
+    title: "Beneath His Bite",
+    author: "Tasha Black",
+    genre: "cozy paranormal shifter",
+    price: 3.99,
+    pages: 260,
+    in_ku: true
+  },
+  {
+    title: "Captured by the Prince",
+    author: "Samantha Dee",
+    genre: "fae fantasy royal intrigue",
+    price: 6.49,
+    pages: 330,
+    in_ku: false
+  },
+  {
+    title: "The Hacker’s Heart",
+    author: "Carmen Fox",
+    genre: "age gap contemporary mafia",
+    price: 4.99,
+    pages: 285,
+    in_ku: true
+  },
+  {
+    title: "Savage Vows",
+    author: "Zita Grant",
+    genre: "dark mafia insta heat",
+    price: 5.29,
+    pages: 320,
+    in_ku: false
+  },
+  {
+    title: "Moonstruck",
+    author: "Tessa Hale",
+    genre: "cozy paranormal werewolf romance",
+    price: 3.75,
+    pages: 270,
+    in_ku: true
+  }
+];
+
+module.exports = booksData;
